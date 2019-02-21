da985debee15dee29883f66f8c4e5bf94fb943a8
Harald Hoyer <harald@redhat.com>
2019-02-21 15:59:52 +0100

    util/mod.rs: add CStrIterator

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,10 +1,11 @@
 pub mod acl;
 pub mod file;
 
-pub use file::{ln_r};
-pub use file::cp;
 pub use acl::acl_copy_fd;
+pub use file::cp;
+pub use file::ln_r;
 
+use std::ffi::CStr;
 use std::io::{self, Read};
 use std::slice;
 
@@ -33,4 +34,33 @@ pub fn read_structs<T, R: Read>(reader: &mut R, num_structs: usize) -> io::Resul
         r.set_len(num_structs);
     }
     Ok(r)
+}
+
+pub struct CStrIterator<'a> {
+    slice: &'a [u8],
+}
+
+impl<'a> CStrIterator<'a> {
+    pub fn from_bytes(slice: &'a [u8]) -> Self {
+        CStrIterator { slice }
+    }
+}
+
+impl<'a> Iterator for CStrIterator<'a> {
+    type Item = &'a CStr;
+    fn next(&mut self) -> Option<Self::Item> {
+        let s = self.slice;
+
+        let mut index = 0usize;
+        #[allow(clippy::explicit_counter_loop)]
+        for elt in s {
+            if *elt == 0 {
+                let (a, b) = s.split_at(index + 1);
+                self.slice = b;
+                return unsafe { Some(CStr::from_bytes_with_nul_unchecked(a)) };
+            }
+            index += 1;
+        }
+        None
+    }
 }
